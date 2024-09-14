17d847ec0dd743525880a5d6df0d14b846eab76a
zhulu@co-mall.com <zhulu@co-mall.com>
2024-09-14 15:58:27 +0800

    vault backup: 2024-09-14 15:58:27
    Affected files:
    笔记/🛠️ 开发/assets/demo-1.js

--- 笔记/🛠️ 开发/assets/demo-1.js
+++ 笔记/🛠️ 开发/assets/demo-1.js
@@ -4,9 +4,9 @@ const stack = [];
 stack.push("东北大板");
 stack.push("光明冰砖");
 //出栈过程
-while (stack.length > 0) {
+while (stack.length) {
   //访问
-  const top = stack(stack.length - 1);
+  const top = stack[stack.length - 1];
   console.log(top);
   //出栈
   stack.pop();
